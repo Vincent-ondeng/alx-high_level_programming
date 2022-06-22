3174b421470ee3c05f5e86523a171848f681a56f
Vincent-ondeng <vincentondeng@gmail.com>
2022-06-21 18:30:54 -0700

    square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,30 +23,30 @@ class Square():
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        @property
-        def size(self):
+    @property
+    def size(self):
             """ getter function for private attribute size.
             Returns:
             size.
             """
-            return self.__size
+        return self.__size
 
-        @size.setter
-        def size(self, value):
+    @size.setter
+    def size(self, value):
             """ setter function for private attribute size.
             Args:
             value: value to be set.
             Returns:
             nothing.
             """
-            if not (isinstance(value, int)):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+        if not (isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-            def area(self):
+    def area(self):
                 """
                 area of the square.
                 """
-                return self.__size * self.__size
+        return self.__size * self.__size
