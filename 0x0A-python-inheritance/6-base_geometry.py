4486842af90888f2a5ac95a59b8a4eebee67f39c
Vincent-ondeng <vincentondeng@gmail.com>
2022-07-04 22:53:23 +0300

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent Base Geometry."""
+
+    def area(self):
+        """Not Implemented."""
+        raise Exception("area() is not implemented")
