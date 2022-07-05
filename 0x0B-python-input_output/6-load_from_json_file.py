9eeeffb88af1ecb663b7e29d7fdc5236763b39a3
Vincent-ondeng <vincentondeng@gmail.com>
2022-07-05 16:43:19 +0300

    I/O

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""6-load_from_json_file: load_from_json_file()
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    loads an object from json file.
+    Args:
+    filename (str): name of file
+    """
+    with open(filename, "r") as j_file:
+        my_obj = json.load(j_file)
+        return my_obj
