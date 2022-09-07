6ebaa9f73075f5f624280b00aaf381193485d2ce
Vincent-ondeng <vincentondeng@gmail.com>
2022-09-07 05:02:54 +0300

    js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
+const SquareP = require('./5-square');
 
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let s = '';
-        for (let j = 0; j < this.width; j++) {
-          s += c;
-        }
-        console.log(s);
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
-};
+}
+
+module.exports = Square;
