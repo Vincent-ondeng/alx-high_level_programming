6257e9f7c43c188830534b834070e5c9493366d0
Vincent-ondeng <vincentondeng@gmail.com>
2022-10-13 01:12:36 -0700

    sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- Prints the full description of the table first in my MySQL server
-SHOW CREATE TABLE; 'first_table';[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C 'first_table';
+SHOW CREATE TABLE `first_table`;
