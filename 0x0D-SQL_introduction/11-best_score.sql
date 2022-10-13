59ef6c815e11148123c6aeda1979f856f5b98469
Vincent-ondeng <vincentondeng@gmail.com>
2022-10-13 11:35:54 +0300

    sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
--- Lists all record in the table second_table witha score >= in my SQL SERVER.
+-- Lists all records in the table second_table with a score >= 10.
 -- Records are ordered by descending score.
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'score' >= 10
-ORDER BY 'score' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
